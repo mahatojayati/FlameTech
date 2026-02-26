7877b6d385203a921e215afd019580d2da2f6f7c
Jayati Mahato <168258529+mahatojayati@users.noreply.github.com>
2026-02-26 14:06:15 +0530

    Delete finance directory

--- finance/compound_interest.py
+++
@@ -1,3 +0,0 @@
-def calculate_compound_interest(p: float, r: float, t: int) -> float:
-    amount = p * ((1 + r/100) ** t)
-    return amount - p
--- finance/simple_interest.py
+++
@@ -1,7 +0,0 @@
-from vedic_finance_engine.core.nikhilam import nikhilam_multiply
-
-def calculate_simple_interest(p: float, r: float, t: float) -> float:
-    product = nikhilam_multiply(int(p), int(r))
-    total = nikhilam_multiply(product, int(t))
-    return total / 100
-
